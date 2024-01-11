f2c8e8201432eae0825031fdbeea0e525b8178f9
Georgii <georgiitimofeev@mail.ru>
2024-01-11 18:25:41 +0300

    Рефакторинг функции

--- plita.py
+++ plita.py
@@ -1,67 +1,62 @@
 import tkinter as tk
 from tkinter import ttk
 
-# объявляем список с формулами, пользователь на экране должен выбрать одну из них
-formuls = ('N1+Fq1', 'N1-Fq+Fq1', 'N2-N1-Fq-Fq1')
 
-# создаем пустой список меток для последующего удаления старых
-labels = []
+def processing_entries(entries: dict) -> dict:
+    """функция для обработки полученных значений. Принимает словарь значений ввёденных пользователем и возвращает
+    новый словарь со зачениями типа float и формулой для расчёта F."""
 
-# создаем словарь единиц измерения переменных
-units = {"hox": "м", "hoy": "м", "h0": "м", "Aq": "м2", "Fq": "кН", "Aq1": "м2", "Fq1": "кН",
-         "F": "кН", "a": "м", "b": "м", "Ub": "м", "Fb_ult": "кН", "Us": "м", "Rsw": "кПа", "qsw": "кН/м",
-         "F_sw_ult": "кН"}
+    values = {}  # создаем словарь для выходных значений
 
-
-def get_entry_values(entries):  # получаем значения введенные пользователем
-
-    # создаем пустой словарь для значений
-    values = {}
-    # перебираем ключи словаря entries
-    for key, entry in entries.items():
-        # отдельно обрабатываем и сразу считаем F
-        if key == "F":
-
-            F_str = entry.get()  # получаем выбранную пользователем формулу
-
-            # получаем значения нужные для рассчета формулы
-            q = float(entries["q"].get().replace(",", "."))         # нагрузка действующая на плиту в пределах площади Aq
-            a1 = float(entries["a1"].get().replace(",", "."))       # размеры стороны поперечного сечения колонны
-            b1 = float(entries["b1"].get().replace(",", "."))
-            zox = float(entries["zox"].get().replace(",", "."))     # защитный слой плиты для продольной и поперечной арматуры, расположенной в направлении взаимно перпендикулярных осей X и Y
-            zoy = float(entries["zoy"].get().replace(",", "."))
-            N1 = float(entries["N1"].get().replace(",", "."))       # продольные силы действующие в колоннах под и над плитой в поперечных сечениях у граней плиты
-            N2 = float(entries["N2"].get().replace(",", "."))
-            q1 = float(entries["q1"].get().replace(",", "."))       # нагрузка от собственного веса плиты
-            Asw = float(entries["Asw"].get().replace(",", "."))     # площадь сечения поперечной арматуры с шагом Sw, расположенная в пределах расстояния 1/2*h0 по обе стороны контура # расчетного поперечного сечения плиты
-            Sw = float(entries["Sw"].get().replace(",", "."))       # Шаг арматуры вдоль расчетного контура
-            Rs = float(entries["Rs"].get().replace(",", "."))       # Расчетное сопротивление арматуры
-            Rbt = float(entries["Rbt"].get().replace(",", "."))     # Расчетное сопротивление бетона
-            h = float(entries["h"].get().replace(",", "."))         # Толщина плиты
-
-            # прописываем формулы нужные для нахождения F
-            hox = h - zox                   # рабочая высота сечений плиты для продольной арматуры, расположенной в направлении взаимно перпендикулярных осей X и Y
-            hoy = h - zoy
-            h0 = 1 / 2 * (hox + hoy)
-            Aq = h0 * (a1 + b1 + h0)        # площадь зоны продавливания расположенная вокруг колонны на расстоянии 1/2*h0 от контура поперечного сечения колонны
-            Fq = q * Aq                     #нормальная сила от разгружающего действия нагрузки на плиту в пределах зоны продавливания, ограниченной расчетным контуром
-            Aq1 = (a1 + h0) * (b1 + h0)     # площадь зоны плиты, ограниченная расчетным контуром, расположенным вокруг колонн на расстоянии 1/2*h0 от контура поперечного сечения колонны
-            Fq1 = q1 * Aq1                  # нормальная сила от разгружающего действия нагрузки, от собственного веса плиты между верхними и нижними колоннами, огранич расчетным контуром
-
-            # Считаем саму формулу
-            if F_str == "N1+Fq1":
-                F_value = N1 + Fq1              # при сопряжении плиты с колонной, расположенной над плитой
-            elif F_str == "N1-Fq+Fq1":
-                F_value = N1 - Fq + Fq1         # при сопряжении фундаментной плиты с колонной
-            elif F_str == "N2-N1-Fq-Fq1":
-                F_value = N2 - N1 - Fq - Fq1    #сосредоточенная нормальная сила F при сопряжении плиты с колоннами, расположенными над и под плитой
-            # добавляем к словарю
-            values[key] = float(F_value)
-        # для остальных данных просто преобразовывваем в число и записываем в словарь значений
+    for key, value in entries.items():  # перебираем словарь entries
+        if key == 'F':
+            values['F'] = value.get()
         else:
-            # получаем значение из поля ввода и преобразуем его в число
-            values[key] = float(entry.get().replace(",", "."))
-    # возвращаем словарь значений
+            values[key] = float(
+                value.get().replace(',', '.'))  # приводим значения к типу float и записываем в словарь values
+
+    return values
+
+
+def search_more_values_for_F(values: dict) -> dict:
+    """Функция для поиска и записи вспомогательных значений для послудующего поиска F. Формулы берутся из методички
+    НИИЖБ по расчёту на продавливание. Принимает на вход словарь значений и дополняет его значениями из формул."""
+
+    values['hox'] = values['h'] - values['zox']  # рабочая высота сечений плиты
+    values['hoy'] = values['h'] - values['zoy']
+    values['h0'] = 1 / 2 * (values['hox'] + values['hoy'])  # расчётная рабочая высота попер. сеч. плиты
+    values['Aq'] = values['h0'] * (values['a1'] + values['b1'] + values['h0'])
+    values['Fq'] = values['q'] * values['Aq']  # нормальная сила от разгр. действия реакт. давл. грунта
+    values['Aq1'] = (values['a1'] + values['h0']) * (values['b1'] + values['h0'])  # площадь зоны плиты, ограниченная
+    # расчётным контуром
+    values['Fq1'] = values['q1'] * values['Aq1']  # нормальная сила от догруж. действия нагрузки от собств. веса
+
+    return values
+
+
+def search_F_value(values: dict) -> dict:
+    """Функция для расчёта и добавления в словарь значений переменной F_value. Выбирает из трёх формул, предложенных
+    пользователю. Принимает словарь значений и возращает его обновленную версию."""
+
+    if values['F'] == 'N1+Fq1':
+        values['F_value'] = values['N1'] + values['Fq1']
+    elif values['F'] == 'N1-Fq+Fq1':
+        values['F_value'] = values['N1'] - values['Fq'] + values['Fq1']
+    elif values['F'] == 'N2-N1-Fq-Fq1':
+        values['F_value'] = values['N2'] - values['N1'] - values['Fq'] - values['Fq1']
+
+    return values
+
+
+def start_working(entries: dict) -> dict:
+    """Функция запускающая работу других функций от начала и до расчёта F_value. Принимает словарь значений
+    полученных от пользователя для передачи в функцию processing_entries. На выходе получается полный словарь
+    преобразованных и расчитанных значений вплоть до F_value."""
+
+    values = processing_entries(entries)
+    values = search_more_values_for_F(values)
+    values = search_F_value(values)
+
     return values
 
 
@@ -83,9 +78,9 @@ def calculate(values):  # считаем по формулам
     h = values['h']
 
     # переводим нужные данные
-    Asw = Asw / (10**4) # в м2
-    Rs = Rs * 1000 # в кн/м2
-    Rbt = Rbt * 1000 # в кн/м2
+    Asw = Asw / (10 ** 4)  # в м2
+    Rs = Rs * 1000  # в кн/м2
+    Rbt = Rbt * 1000  # в кн/м2
 
     # прописываем и считаем формулы
     hox = h - zox
@@ -95,7 +90,7 @@ def calculate(values):  # считаем по формулам
     Fq = q * Aq
     Aq1 = (a1 + h0) * (b1 + h0)
     Fq1 = q1 * Aq1
-    F = values['F'] # получаем F из словаря
+    F_value = values['F_value']  # получаем F из словаря
     a = a1 + h0
     b = b1 + h0
     Ub = 2 * (a + b)
@@ -105,22 +100,23 @@ def calculate(values):  # считаем по формулам
 
     # записываем полученные значения в словарь
     results["hox"] = hox
+
     results["hoy"] = hoy
     results["h0"] = h0
     results["Aq"] = Aq
     results["Fq"] = Fq
     results["Aq1"] = Aq1
     results["Fq1"] = Fq1
-    results["F"] = F
+    results["F"] = F_value
     results["a"] = a
     results["b"] = b
     results["Ub"] = Ub
     results["Fb_ult"] = Fb_ult
 
     if Fb_ult != 0:  # если Fb_ult равно нулю, процентные соотношения не считаются, чтобы не вызвать ошибку деления на 0!
-        F_Fb_ult = int(F / Fb_ult * 100)  # % соотношение F от Fb_ult
+        F_Fb_ult = int(F_value / Fb_ult * 100)  # % соотношение F от Fb_ult
 
-    if F <= Fb_ult:  # проверяем условие и если оно пройдено, прекращаем работу программы и дальнейший расчет.
+    if F_value <= Fb_ult:  # проверяем условие и если оно пройдено, прекращаем работу программы и дальнейший расчет.
         if Fb_ult != 0:
             results[
                 "Fb_check"] = f"Несущая способность удовлетворительна, арматура не нужна. F это {F_Fb_ult}% от Fb_ult"
@@ -134,15 +130,17 @@ def calculate(values):  # считаем по формулам
         results['Fb_check'] = 'Несущая способность неудовлетворительна, добавляем арматуру.'
 
         # добавляем формулы для арматуры
-    # добавляем формулы для арматуры
         Us = Ub
-        Rsw  = 0.8 * Rs
+        Rsw = 0.8 * Rs
         if Rsw > 300000:  # Rsw не может быть больше 300000мПа
             Rsw = 300000
         qsw = (Rsw * Asw) / Sw
         F_sw_ult = 0.8 * qsw * Us
 
-        F_sw_ult_Fb_ult = int(F_sw_ult / Fb_ult * 100)  # % соотношение F_sw_ult от Fb_ult
+        try:
+            F_sw_ult_Fb_ult = int(F_sw_ult / Fb_ult * 100)  # % соотношение F_sw_ult от Fb_ult
+        except ZeroDivisionError:
+            pass
 
         if F_sw_ult > Fb_ult:
             F_sw_ult = Fb_ult
@@ -164,7 +162,7 @@ def calculate(values):  # считаем по формулам
 
         if F_sw_ult > 0.5 * Fb_ult:
             results["F_sw_ult_half_check"] = "F_sw_ult > 0.5Fb_ult, проверка ПРОЙДЕНА!"
-            if F <= Fb_ult + F_sw_ult:
+            if F_value <= Fb_ult + F_sw_ult:
                 results[
                     "F_strength_check"] = "F <= Fb_ult + F_sw_ult, условие прочности выполнено, прочность ОБЕСПЕЧЕНА!"
             else:
@@ -175,25 +173,19 @@ def calculate(values):  # считаем по формулам
             results[
                 "F_sw_ult_half_check"] = "F_sw_ult < 0.5Fb_ult, условие не выполняется, арматура не учитывается в расчете!"
 
-        if F <= Fb_ult:
-            results["F_strength_check"] = "F <= Fb_ult , условие прочности выполнено, прочность ОБЕСПЕЧЕНА!"
-        else:
-            results["F_strength_check"] ="F => Fb_ult , Условие прочности не выполнено. Прочность не обеспечена!"
-
-    # добавляем еще результаты
-    results["Us"] = Us
-    results["Rsw"] = Rsw
-    results["qsw"] = qsw
-    results["F_sw_ult"] = F_sw_ult
+            if F_value <= Fb_ult:
+                results["F_strength_check"] = "F <= Fb_ult, условие прочности выполнено, прочность ОБЕСПЕЧЕНА!"
+            else:
+                results["F_strength_check"] = "F => Fb_ult, Условие прочности не выполнено. Прочность не обеспечена!"
 
     return results
 
 
 def show_values():  # выводим результаты на экран приложения
-    values = get_entry_values(entries)  # получаем словарь значений
-    ##    print(values) # выводим в консоль для проверки
+    values = start_working(entries)  # получаем словарь значений
+    print(values)
     results = calculate(values)  # считаем с помощью функции calculate
-    ##    print(results) # выводим в консоль для проверки
+    print(results)
     global labels  # создаем глобальную переменную для хранения меток
     if labels:  # если список меток не пустой
         for label in labels:  # удаляем каждую метку с экрана
@@ -203,30 +195,19 @@ def show_values():  # выводим результаты на экран при
         if isinstance(value, (int, float)):  # проверяем, является ли значение числом
             value = round(value, 3)  # округляем значение до трех знаков после запятой
             unit = units[key]  # получаем единицу измерения из словаря units по ключу
-            label = tk.Label(win, text = f"{key}: {value} {unit}")
+            label = tk.Label(win, text=f"{key}: {value} {unit}")
         else:
-            label = tk.Label(win, text = f"{value}", font=("Arial", 10, "bold"))
+            label = tk.Label(win, text=f"{value}", font=("Arial", 10, "bold"))
         label.grid(row=i + 1, column=3, sticky="w")
         labels.append(label)  # добавляем метку в список
 
-def create_window():
-    win = tk.Tk() # создаем окно приложения
-    ##photo = tk.PhotoImage(file='fit.png') # пока не используем иконку приложения для простоты тестов на разных устройствах
-    ##win.iconphoto(False, photo)
-    win.title('Расчёт плиты на продавливание') # задаем название вверху окна
-    width = 1100  # ширина окна в пикселях
-    height = 600  # высота окна в пикселях
-    x = 250  # координата x левого верхнего угла окна
-    y = 50  # координата y левого верхнего угла окна
-    win.geometry(f'{width}x{height}+{x}+{y}')  # задаем размер и адрес окна
-    win.geometry('1100x600+250+50')  # задаем размер и адрес окна
-    win.resizable(False, False)  # запрещаем менять размер
-    # задаем размеры столбцов
-    win.grid_columnconfigure(0, minsize=200)
-    win.grid_columnconfigure(0, minsize=100)
-    return win
 
-win = create_window()
+win = tk.Tk()  # создаем окно приложения
+##photo = tk.PhotoImage(file='fit.png') # пока не используем иконку приложения для простоты тестов на разных устройствах
+##win.iconphoto(False, photo)
+win.title('Расчёт плиты на продавливание')  # задаем название вверху окна
+win.geometry('1100x600+250+50')  # задаем размер и адрес окна
+win.resizable(False, False)  # запрещаем менять размер
 
 # Создаем окна для ввода данных с клавиатуры
 label_1 = tk.Label(win, text='Введите данные согласно указанным единицам: ', justify='center', font='bold')
@@ -299,6 +280,8 @@ h.grid(row=13, column=1)
 
 label_15 = tk.Label(win, text='Тип сопряжения перекрытия с колонной (формула), F = ')
 label_15.grid(row=14, column=0, stick='e')
+formuls = ('N1+Fq1', 'N1-Fq+Fq1',
+           'N2-N1-Fq-Fq1')  # объявляем список с формулами, пользователь на экране должен выбрать одну из них
 F = ttk.Combobox(win, values=formuls)
 F.grid(row=14, column=1)
 
@@ -306,8 +289,18 @@ F.grid(row=14, column=1)
 entries = {"q": q, "a1": a1, "b1": b1, "zox": zox, "zoy": zoy, "N1": N1, "N2": N2, "q1": q1, "Asw": Asw, "Sw": Sw,
            "Rs": Rs, "Rbt": Rbt, "h": h, "F": F}
 
-btn_start = tk.Button(win, text = 'Рассчитать', command=show_values, bg='orange',activebackground='orange',bd=5) # создаем кнопку "рассчитать" которая запускает расчет
+labels = []  # создаем пустой список меток для последующего удаления старых
+
+units = {"hox": "м", "hoy": "м", "h0": "м", "Aq": "м2", "Fq": "кН", "Aq1": "м2", "Fq1": "кН",
+         "F": "кН", "a": "м", "b": "м", "Ub": "м", "Fb_ult": "кН", "Us": "м", "Rsw": "кПа", "qsw": "кН/м",
+         "F_sw_ult": "кН"}
+
+btn_start = tk.Button(win, text='Рассчитать', command=show_values, bg='orange', activebackground='orange',
+                      bd=5)  # создаем кнопку "рассчитать" которая запускает расчет
 btn_start.grid(row=15, column=0, columnspan=2, stick='we')
 
+# задаем размеры столбцов
+win.grid_columnconfigure(0, minsize=200)
+win.grid_columnconfigure(0, minsize=100)
 
 win.mainloop()
